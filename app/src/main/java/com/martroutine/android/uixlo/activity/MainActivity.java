88f91f4a3a6c1dea0470e808f8a36c98148edcd3
Vtanathip <tanathip.se@gmail.com>
2014-06-04 23:47:39 +0700

    remove conflict code from libs

--- app/src/main/java/com/martroutine/android/uixlo/activity/MainActivity.java
+++ app/src/main/java/com/martroutine/android/uixlo/activity/MainActivity.java
@@ -1,12 +1,9 @@
 package com.martroutine.android.uixlo.activity;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,7 +12,7 @@ import com.martroutine.android.ui_xlo.app.R;
 import javax.inject.Inject;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseUIXLOActivity {
 
     @Inject
     public SharedPreferences sharedPreferences;
@@ -23,32 +20,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         } 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
