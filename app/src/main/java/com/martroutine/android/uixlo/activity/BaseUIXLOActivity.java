6597d0e93e03756f1379a4b47e947babb8237e3d
Vtanathip <tanathip.se@gmail.com>
2014-06-04 23:47:04 +0700

    TAG application for check Logcat

--- app/src/main/java/com/martroutine/android/uixlo/activity/BaseUIXLOActivity.java
+++ app/src/main/java/com/martroutine/android/uixlo/activity/BaseUIXLOActivity.java
@@ -6,6 +6,7 @@ import com.martroutine.android.common.skeleton.activity.BaseActivity;
  * Created by MARTROUTINE on 04/06/2014.
  */
 public class BaseUIXLOActivity extends BaseActivity {
+    public static final String TAG = "UIXLO";
     @Override
     protected Object[] geActivitytModules() {
         return new Object[]{
