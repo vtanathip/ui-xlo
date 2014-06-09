73014a84605d18038eb44cb26df2e59720e6beea
Vtanathip <tanathip.se@gmail.com>
2014-06-09 22:43:28 +0700

    Bypass instruction page to main page. (It will show instruction page all the time)

--- app/src/main/java/com/martroutine/android/uixlo/fragment/InstructionWelcome.java
+++ app/src/main/java/com/martroutine/android/uixlo/fragment/InstructionWelcome.java
@@ -1,5 +1,6 @@
 package com.martroutine.android.uixlo.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,8 @@ import android.view.ViewGroup;
 
 import com.martroutine.android.common.skeleton.fragment.BaseFragment;
 import com.martroutine.android.ui_xlo.app.R;
+import com.martroutine.android.uixlo.activity.MainActivity;
+import com.martroutine.android.uixlo.custom.CustomButton;
 
 /**
  * Created by MARTROUTINE on 08/06/2014.
@@ -14,7 +17,18 @@ import com.martroutine.android.ui_xlo.app.R;
 public class InstructionWelcome extends BaseFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.viewpagger_welcome, container, false);
+
+        View inflateView = inflater.inflate(R.layout.viewpagger_welcome, container, false);
+        CustomButton customButton = (CustomButton) inflateView.findViewById(R.id.instruction_welcome_button);
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return inflateView;
     }
 
 }
